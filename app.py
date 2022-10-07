6e70af24f1421c6fb27130c137b325237a5dd245
EsGarciaC <esgarciac@unal.edu.co>
2022-10-07 11:01:32 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pandas as pd
 import pydeck as pdk
 import joblib
 
-st.set_page_config(page_title="", page_icon="./Graficas/stockfish.png", layout="centered", initial_sidebar_state="auto")
+st.set_page_config(page_title="miDataCreditoGratis.com", page_icon="./Graficas/stockfish.png", layout="centered", initial_sidebar_state="auto")
 
 
 
@@ -21,6 +21,6 @@ df_data = load_data()
 
 ########### SIDEBAR ##########
 with st.sidebar:
-
+    pass
     
 #############################
