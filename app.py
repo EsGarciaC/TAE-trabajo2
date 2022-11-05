8c5537b272916d7e0b28af8e52bc2ed8c1d46061
EsGarciaC <esgarciac@unal.edu.co>
2022-11-05 17:42:15 -0500

    Update app.py

--- app.py
+++ app.py
@@ -10,8 +10,13 @@ st.set_page_config(page_title="miDataCreditoGratis.com", page_icon="./DataFrames
 
 st.title("miDataCreditoGratis.com")
 
+st.markdown("¡Infórmate sobre la página! https://www.youtube.com/watch?v=0QM6W8hIw54&t=11s")
+
 st.markdown("""¡Bienvenido! Esta aplicación le ayudará a averiguar su puntaje 
-            crediticio de manera totalmenta gratuita. Para comenzar, simplemente 
+            crediticio de manera totalmenta gratuita. Conocer su puntaje crediticio es útil para
+            aquellos que estén pensando en adquirir un préstamo, 
+            una tarjeta de crédito o simplemente estén interesados en la confianza que generan
+            en las entidades financieras. Para comenzar, simplemente 
             rellene el cuestionario de la izquierda con su información financiera básica. En la parte de abajo podrá visualizar su puntaje crediticio, lo que implica y 
             cómo éste se compara con el de la persona promedio.""")
 
@@ -249,5 +254,6 @@ else:
 
 st.markdown("Rangos adaptados de: *experian.com*")
 
+st.markdown("Para detalles técnicos sobre el desarrollo de la aplicación, dirigirse a: https://github.com/EsGarciaC/TAE-trabajo2")
 
         
